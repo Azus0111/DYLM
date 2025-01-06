6b5d9c70136305ba0748cec753727509038e34d3
Azus0111 <huynhnhatlam391@gmail.com>
2025-01-05 22:00:11 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const yesButton = document.getElementById('yes')
+const noButton = document.getElementById('no')
+yesButton.addEventListener('click', () => (
+    alert('Omg! I love you, too')
+))
+noButton.addEventListener('mouseover', () => {
+    noButton.style.position = 'absolute'
+    let newX = Math.floor(Math.random()*window.innerWidth)
+    let newY = Math.floor(Math.random()*window.innerHeight)
+    noButton.style.left = newX + 'px'
+    noButton.style.top = newY + 'px'
+})
